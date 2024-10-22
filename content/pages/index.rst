73d8a9338e46ccaa9df6f28e432ec0ec1101a712
Jonathan Demaeyer <jodemaey@meteo.be>
2024-10-22 17:09:05 +0200

    First page and test

---
+++ content/pages/index.rst
@@ -0,0 +1,9 @@
+The EUMETNET EUPP benchmark
+===========================
+
+:date: 2024-10-22 15:00
+:summary: EUPP Benchmark main page
+:status: hidden
+:save_as: index.html
+
+Work in progress
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,7 @@
 AUTHOR = 'EUMETNET PP Module'
 SITENAME = 'EUMETNET EUPP Benchmark'
 SITEURL = ""
+SITETITLE = "EUPP Benchmark"
 
 PATH = "content"
 
@@ -16,13 +17,13 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
-
+# LINKS = (
+#     ("Pelican", "https://getpelican.com/"),
+#     ("Python.org", "https://www.python.org/"),
+#     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+#     ("You can modify those links in your config file", "#"),
+# )
+#
 # Social widget
 SOCIAL = (
     ("You can add links in your config file", "#"),
@@ -33,3 +34,55 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+THEME = "/home/jodemaey/pelican-themes/Flex"
+BROWSER_COLOR = "#94c1cf"
+SITEDESCRIPTION = "Presentation and News about the EUPP Benchmark activities"
+SITELOGO = SITEURL + "images/EUMETNETLogo.png"
+FAVICON = SITEURL + "/images/EUMETNETLogo.png"
+
+ROBOTS = "index, follow"
+
+CC_LICENSE = {
+    "name": "Creative Commons Attribution-ShareAlike",
+    "version": "4.0",
+    "slug": "by-sa"
+}
+
+COPYRIGHT_YEAR = 2024
+
+# STATIC_PATHS = ["extra/custom.css"]
+#
+# EXTRA_PATH_METADATA = {
+#     "extra/custom.css": {"path": "static/custom.css"},
+# }
+#
+# CUSTOM_CSS = "static/custom.css"
+
+MAIN_MENU = True
+
+# ADD_THIS_ID = "ra-77hh6723hhjd"
+# DISQUS_SITENAME = "yoursite"
+# GOOGLE_ANALYTICS = "UA-1234-5678"
+# GOOGLE_TAG_MANAGER = "GTM-ABCDEF"
+
+# # Enable i18n plugin.
+# PLUGINS = ["i18n_subsites"]
+# # Enable Jinja2 i18n extension used to parse translations.
+# JINJA_ENVIRONMENT = {"extensions": ["jinja2.ext.i18n"]}
+#
+# # Translate to German.
+# DEFAULT_LANG = "de"
+# OG_LOCALE = "de_DE"
+# LOCALE = "de_DE"
+#
+# # Default theme language.
+# I18N_TEMPLATES_LANG = "en"
+#
+# # Pelican-search Configuration
+# STORK_INPUT_OPTIONS = {"stemming": "German", "url_prefix": SITEURL}
+
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True
+
+GITHUB_CORNER_URL = "https://github.com/EUPP-benchmark"
