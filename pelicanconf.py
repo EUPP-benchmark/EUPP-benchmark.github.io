abe5d7f9b99167e0ad6cd5b8c434b7117da87713
Jonathan Demaeyer <jodemaey@meteo.be>
2024-10-23 10:32:03 +0200

    Adding link to datasets documentation

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,13 +17,14 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-# LINKS = (
-#     ("Pelican", "https://getpelican.com/"),
-#     ("Python.org", "https://www.python.org/"),
-#     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-#     ("You can modify those links in your config file", "#"),
-# )
-#
+LINKS = (
+    ("Datasets documentation", "https://eupp-benchmark.github.io/EUPPBench-doc/"),
+    # ("Pelican", "https://getpelican.com/"),
+    # ("Python.org", "https://www.python.org/"),
+    # ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    # ("You can modify those links in your config file", "#"),
+)
+
 # Social widget
 SOCIAL = (
     ("You can add links in your config file", "#"),
